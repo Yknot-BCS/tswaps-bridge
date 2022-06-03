8f31e549848e804b9cf790247a490984da9164c2
Ja-Q <jpbeukes27@gmail.com>
2022-06-03 12:31:58 +0200

    fix: remove sign and sqrl from ual

--- src/boot/ual.js
+++ src/boot/ual.js
@@ -4,15 +4,18 @@ import { Scatter } from "ual-scatter";
 import { Wombat } from "ual-wombat";
 import { Sqrl } from "@smontero/ual-sqrl";
 import { Anchor } from "ual-anchor";
-import {Wax} from "@eosdacio/ual-wax";
+import { Wax } from "@eosdacio/ual-wax";
 
 export default async ({ Vue, store }) => {
   if (localStorage.getItem("selectedChain") != null) {
-    await store.dispatch("blockchains/updateCurrentChain", localStorage.getItem("selectedChain"))
+    await store.dispatch(
+      "blockchains/updateCurrentChain",
+      localStorage.getItem("selectedChain")
+    );
   } else {
-    await store.dispatch("blockchains/updateCurrentChain", "TELOS")
+    await store.dispatch("blockchains/updateCurrentChain", "TELOS");
   }
-  let getCurrentChain = store.getters['blockchains/getCurrentChain'];
+  let getCurrentChain = store.getters["blockchains/getCurrentChain"];
   const chain = {
     chainId: getCurrentChain.NETWORK_CHAIN_ID,
     rpcEndpoints: [
@@ -24,20 +27,22 @@ export default async ({ Vue, store }) => {
     ]
   };
 
-  let authenticators = []
+  let authenticators = [];
 
   // if telos network, include 'telos sign' as login option
-  if (getCurrentChain.NETWORK_NAME === 'TELOS') {
-    authenticators = authenticators.concat([new KeycatAuthenticator([chain], { appName: process.env.APP_NAME })])
-  }
+  // if (getCurrentChain.NETWORK_NAME === 'TELOS') {
+  //   authenticators = authenticators.concat([new KeycatAuthenticator([chain], { appName: process.env.APP_NAME })])
+  // }
 
   // if wax network, include 'wax cloud wallet' as login option
-  if (getCurrentChain.NETWORK_NAME === 'WAX') {
-    authenticators = authenticators.concat([new Wax([chain], { appName: process.env.APP_NAME })])
+  if (getCurrentChain.NETWORK_NAME === "WAX") {
+    authenticators = authenticators.concat([
+      new Wax([chain], { appName: process.env.APP_NAME })
+    ]);
   }
 
   authenticators = authenticators.concat([
-    new Sqrl([chain], { appName: process.env.APP_NAME }),
+    // new Sqrl([chain], { appName: process.env.APP_NAME }),
     new Anchor([chain], { appName: process.env.APP_NAME }),
     new Wombat([chain], { appName: process.env.APP_NAME }),
     new Scatter([chain], { appName: process.env.APP_NAME })
